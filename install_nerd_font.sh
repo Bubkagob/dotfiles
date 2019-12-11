d18db0ff1c05db90ac058edd6241944f0c696f5a
Ivan Alexandrov <ivan.alexandrov@syntacore.com>
2019-12-11 12:04:50 +0300

    changed install NERD font

--- install_nerd_font.sh
+++ install_nerd_font.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-wget https://github.com/source-foundry/Hack/releases/download/v3.003/Hack-v3.003-ttf.zip
-mkdir -p ~/.local/share/fonts/Hack
-unzip -q Hack-v4.003-ttf.zip -d ~/.local/share/fonts/HackAjlk
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
 fc-cache -f -v
-rm *.zip*
